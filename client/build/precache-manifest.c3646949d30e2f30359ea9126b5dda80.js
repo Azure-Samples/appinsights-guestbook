43049c7b72bbf8a18e5eb5a692341e92fcfc217c
Mark Wolff <mark.wolff@microsoft.com>
2019-02-01 12:08:08 -0800

    update react-scripts

---
+++ client/build/precache-manifest.c3646949d30e2f30359ea9126b5dda80.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c797c699d5f289c796e2",
+    "url": "/static/css/main.581d3118.chunk.css"
+  },
+  {
+    "revision": "c797c699d5f289c796e2",
+    "url": "/static/js/main.c797c699.chunk.js"
+  },
+  {
+    "revision": "78bcf9ed193c778be37e",
+    "url": "/static/css/1.8bf190b4.chunk.css"
+  },
+  {
+    "revision": "78bcf9ed193c778be37e",
+    "url": "/static/js/1.78bcf9ed.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "8958c57f406f2031ccc01171e33f4fc4",
+    "url": "/index.html"
+  }
+];
